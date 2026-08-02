82dcaab2d9cf0c86375a529ff093ae3178af3410
Eduardo Alves <155503937+4vinho@users.noreply.github.com>
2026-08-02 17:57:40 -0300

    Update TemplateVagaConfiguration.java

--- src/main/java/br/com/pacto/recrutamento/infra/configurations/TemplateVagaConfiguration.java
+++ src/main/java/br/com/pacto/recrutamento/infra/configurations/TemplateVagaConfiguration.java
@@ -6,6 +6,8 @@ import br.com.pacto.recrutamento.infra.repositorys.templatevaga.AutorizacaoTempl
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.UUID;
+
 @Configuration
 class TemplateVagaConfiguration {
     @Bean
